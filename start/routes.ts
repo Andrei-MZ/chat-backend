034416ab910a86cf5110df30439a77e28b3e9325
andrei <andreimendes8@gmail.com>
2024-11-27 09:18:13 -0300

    feat: rotas

--- start/routes.ts
+++ start/routes.ts
@@ -1,16 +1,19 @@
-/*
-|--------------------------------------------------------------------------
-| Routes file
-|--------------------------------------------------------------------------
-|
-| The routes file is used for defining the HTTP routes.
-|
-*/
+import Route from '@ioc:Adonis/Core/Route'
 
-import router from '@adonisjs/core/services/router'
+Route.get('/', async () => {
+  return { hello: 'world' }
+})
 
-router.get('/', async () => {
-  return {
-    hello: 'world',
-  }
+Route.post('/users/register', 'AuthController.register')
+Route.post('/users/login', 'AuthController.login')
+
+Route.group(() => {
+  Route.get('/users', 'UsersController.index')
+  Route.get('/users/:id', 'UsersController.show')
+  Route.put('/users/:id', 'UsersController.update')
+  Route.delete('/users/:id', 'UsersController.destroy')
+}).middleware('auth')
+
+Route.get('/github/redirect', async ({ ally }) => {
+  return ally.use('github').redirect()
 })
